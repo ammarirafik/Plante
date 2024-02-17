6c903bbdcd8eac800cb4badd3a32c75dd9036312
Ammari <votre@rafikammri430@gmail.com>
2024-02-17 22:21:32 +0100

    Mise à jour avec les dernières modifications du dépôt distant

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hellooooooo</Text>
+      <Text>hellooooooo fffff</Text>
       <StatusBar style="auto" />
     </View>
   );
